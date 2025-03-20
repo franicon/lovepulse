0d8dec604f5998843173d19e595651bc78386e21
frani <franicons@gmail.com>
2025-03-20 23:11:15 +0100

    update-policy

--- app/terms-of-use/page.tsx
+++ app/terms-of-use/page.tsx
@@ -4,7 +4,7 @@ import Terms from "@/components/Terms";
 export default function TermsOfUse() {
     return (
         <div className="">
-            <Terms/>
+            {/*<Terms/>*/}
 
         </div>
     );
